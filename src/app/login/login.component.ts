3e717e592dbbc460487885243572badfd843ac57
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2020-12-06 23:08:38 +0100

    Implementar Google Login
    En la pagina de Login se coloca un boton y en el ts se implementa login con Google
    
    Para esto debemos usar el modulo AngularFireAuth, esto lo inyectamos en el componente
    
    AngularFireAuth presenta varias alternativas para hacer login
    
    Usamo firebase para usar el GoogleAuthProvider() con el que nos podemos conectar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  ngOnInit(): void {}
+  login(): void {
+    this.afAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider());
+  }
 }
