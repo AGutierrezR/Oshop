6f706a107cc12266f3c9ca31272e6c07f3998417
Andrés Gutiérrez <agutierrez.codes@gmail.com>
2020-12-06 19:11:06 +0100

    Agregar rutas a las paginas
    Se han agregado las rutas en el app-routing.modules.ts, cada ruta apunta a un componente
    
    Dicho componente sera renderizado en el  <router-outlet> que esta en app.component.html
    
    En el bs-navbar se han agregado algunas rutas con el routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminOrdersComponent } from 'src/app/admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from 'src/app/admin/admin-products/admin-products.component';
+import { CheckOutComponent } from 'src/app/check-out/check-out.component';
+import { HomeComponent } from 'src/app/home/home.component';
+import { LoginComponent } from 'src/app/login/login.component';
+import { OrderSuccessComponent } from 'src/app/order-success/order-success.component';
+import { ProductsComponent } from 'src/app/products/products.component';
+import { ShoppingCartComponent } from 'src/app/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'checkout', component: CheckOutComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
