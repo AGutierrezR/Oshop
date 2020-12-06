8333788d2bef579df0a7a1500989133ffd4afa79
Andrés Gutiérrez <agutierrez.codes@gmail.com>
2020-12-06 17:02:25 +0100

    Agregar Firebase
    Se agregaron los modulos de firebase para poder hacer uso de esta plataforma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// FireBase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBkDlvlfKMN_0zw-8cc6dYMqdK7D8dmJl8',
+    authDomain: 'oshop-16568.firebaseapp.com',
+    projectId: 'oshop-16568',
+    storageBucket: 'oshop-16568.appspot.com',
+    messagingSenderId: '632741935286',
+    appId: '1:632741935286:web:a9fa158ffcf3282f368e17',
+    measurementId: 'G-49R1JGR9XC',
+  },
 };
 
 /*
