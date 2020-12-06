e5351810284feb5f8b82daa17922f534ff0cd98a
Andrés Gutiérrez <agutierrez.codes@gmail.com>
2020-12-06 18:55:37 +0100

    Extrayendo el Navbar
    Se ha separado el navbar a un componente, para separar intereses
    
    Aparte se han borrado los .spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BsNavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
