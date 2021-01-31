cd3495a8d50aad0f728de3127810c500d9b3d116
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-01-31 15:01:54 +0100

    Mejorando el ProductCardComponent

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
   @Input() product;
