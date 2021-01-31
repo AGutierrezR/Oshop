f91637484aaab18a96304dc64640db286ad3276e
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-01-31 01:59:52 +0100

    Filtrando productos por categoria

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
 
@@ -7,12 +10,25 @@ import { ProductService } from 'src/app/product.service';
   templateUrl: './products.component.html',
 })
 export class ProductsComponent implements OnInit {
-  products$ = this.productService.getAll();
   categories$ = this.categoryService.getAll();
+  filterCriteria$ = this.route.queryParamMap.pipe(
+    map((params) => params.get('category'))
+  );
+  products$ = combineLatest([
+    this.productService.getAll(),
+    this.filterCriteria$,
+  ]).pipe(
+    map(([products, category]) =>
+      category
+        ? products.filter((product) => product.category === category)
+        : products
+    )
+  );
 
   constructor(
     private productService: ProductService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {}
