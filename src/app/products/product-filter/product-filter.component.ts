43fdbc0fefba58499f9f22f980e7bdf2ddc35908
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-01-31 15:15:54 +0100

    Haciendo a las Categorias Sticky
    Se elimino css innecesario, y se mejoro la diagramacion del navbar

--- src/app/products/product-filter/product-filter.component.ts
+++ src/app/products/product-filter/product-filter.component.ts
@@ -4,6 +4,7 @@ import { CategoryService } from 'src/app/category.service';
 @Component({
   selector: 'product-filter',
   templateUrl: './product-filter.component.html',
+  styleUrls: ['./product-filter.component.scss'],
 })
 export class ProductFilterComponent {
   categories$ = this.categoryService.getAll();
