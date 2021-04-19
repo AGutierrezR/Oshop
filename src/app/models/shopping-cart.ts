da01c833df50b48483da98d1cbfd628f366e0cda
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-04-19 02:30:27 +0200

    Usando Better Object Initialization
    Se puede pasar un objecto como parametro de constructor para hacer la inicializacion algo mas sencillo
    
    Hay que investigar mas sobre esto

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,5 +1,6 @@
 import { Product } from '@models/product';
 import { ShoppingCartItem } from '@models/shopping-cart-item';
+import { ShoppingCartItemMap } from '@models/shopping-cart-item-map';
 
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
@@ -12,15 +13,12 @@ export class ShoppingCart {
     return this.items.reduce((acc, item) => item.totalPrice + acc, 0);
   }
 
-  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+  constructor(public itemsMap: Partial<ShoppingCartItemMap>) {
     for (const productId in itemsMap) {
       if (this.itemsMap[productId]) {
         const item = this.itemsMap[productId];
-        const x = new ShoppingCartItem();
-        Object.assign(x, item);
-        x.$key = productId;
 
-        this.items.push(x);
+        this.items.push(new ShoppingCartItem({ ...item, $key: productId }));
       }
     }
   }
