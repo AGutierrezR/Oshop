5930881fb0add010ffaa617fb2b89b54c16b1f29
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-04-23 16:07:14 +0200

    Limpiando el ShoppingCart
    Para esto, inyectamos ShoppingCartService en OrderService
    
    Esto no daria ningun problema ya que los servicios son Singletons

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Order } from '@models/order';
+import { ShoppingCartService } from 'src/app/shopping-cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  constructor(private db: AngularFireDatabase) {}
+  constructor(
+    private db: AngularFireDatabase,
+    private shoppingCartService: ShoppingCartService
+  ) {}
 
-  storeOrder(order: Order): any {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order: Order): Promise<any> {
+    const result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
   }
 }
