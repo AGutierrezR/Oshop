335ed67e51f134a5fcc1b3b53bd55175b2aa6ed6
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-05-19 11:38:52 +0200

    Implementando nuevas rutas y mejora routerLinks

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  appUser$: Observable<boolean> = this.auth.appUser$.pipe(map((x) => !!x));
+
+  constructor(private auth: AuthService) {}
 
   ngOnInit(): void {}
 }
--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -4,6 +4,15 @@ import { AuthService } from '@core/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styles: [
+    `
+      .btn-google {
+        color: #545454;
+        background-color: #ffffff;
+        box-shadow: 0 1px 2px 1px #ddd;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   constructor(private auth: AuthService) {}
